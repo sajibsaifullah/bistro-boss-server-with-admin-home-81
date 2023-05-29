95797a23a587250b4f54cf02ad328fdfafe799ee
sajibsaifullah <dusajibphy@gmail.com>
2023-05-29 12:31:22 +0600

    admin

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ async function run() {
       res.send(result);
     })
 
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
